4bad2bc74eb5710ce145886a7061ef4b9480d898
Everton Baima <everton-baima@hotmail.com>
2018-05-12 15:29:27 -0300

    Histórico de partidas.

---
+++ TicTacToe/Cells/HistoricoPartidasTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  HistoricoPartidasTableViewCell.swift
+//  TicTacToe
+//
+//  Created by Everton Baima on 12/05/2018.
+//  Copyright © 2018 UNI7. All rights reserved.
+//
+
+import UIKit
+
+public class HistoricoPartidasTableViewCell: UITableViewCell {
+    @IBOutlet weak var resultado: UIImageView!
+    @IBOutlet weak var nomeJogador: UILabel!
+    @IBOutlet weak var dataPartida: UILabel!
+}
